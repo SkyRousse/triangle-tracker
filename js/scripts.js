6c52572fc59343ef357d4ce5f8b7b02de6194ece
Kevin VanEvery and Sky Rousse <sky@>
2016-06-02 13:56:12 -0700

    updated validation to use HTML5 required tag instead of js

--- js/scripts.js
+++ js/scripts.js
@@ -2,24 +2,11 @@
 $(function () {
   $("form#triangle-form").submit(function(event) {
   event.preventDefault();
-  debugger;
   var side1 = parseInt($("input#side1").val());
   var side2 = parseInt($("input#side2").val());
   var side3 = parseInt($("input#side3").val());
 
-  if (!side1) {
-    $("div.side1").addClass("has-error");
-    alert("Please fill in required fields");
-  }
-  else if (!side2) {
-    $("div.side2").addClass("has-error");
-    alert("Please fill in required fields");
-  }
-  else if (!side3) {
-    $("div.side3").addClass("has-error");
-    alert("Please fill in required fields");
-  }
-  else if (side1 + side2 <= side3 || side2 + side3 <= side1 || side1 + side3 <= side2) {
+if (side1 + side2 <= side3 || side2 + side3 <= side1 || side1 + side3 <= side2) {
     $(".output").html("<p>This is not a triangle</p>")
   }
   else if (side1 === side2 && side2 === side3) {
